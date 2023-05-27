f0d8c7dd0d4d045dc887000911457023213820aa
mrbrovki <devbrovki@gmail.com>
2023-05-27 12:06:32 +0200

    login signup routes for users

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,9 +3,14 @@ const router = express.Router();
 const controller = require("../controllers/users");
 
 router.use(express.json());
+
 router.post("/create", controller.create);
 router.get("/:id", controller.get);
 router.put("/update", controller.update);
 router.delete("/delete/:id", controller.delete);
+
+
+router.post("/login", controller.login);
+router.post("/signup", controller.signup);
 
 module.exports = router;
