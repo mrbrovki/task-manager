6980a2290048ea354145bb14c72c40cc037b55f8
mrbrovki <devbrovki@gmail.com>
2023-05-27 12:06:00 +0200

    users can now login and signup

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,9 +1,55 @@
+const sequelize = require("../config/database");
 const User = require("../models/users");
 const { v4: uuidv4 } = require('uuid');
+const jwt = require("jsonwebtoken");
 const bcrypt = require('bcrypt');
-const sequelize = require("../config/database");
+require('dotenv').config();
 
 const saltRounds = 10;
+
+const login = async (req, res) => {
+    try{
+        await sequelize.sync();
+
+        const {email, password} = req.body;
+        
+
+        const user = await User.findOne({email});
+        const isValid = await bcrypt.compare(password, user.password);
+        
+        if(!isValid){
+            res.status(401).json({ error: 'Invalid credentials' });
+        }
+
+        const token = jwt.sign({ userId: user.id }, "fsdfsdfsafsafas", { expiresIn: '1h' });
+
+        res.json({ token });
+
+    }catch(err){
+        console.error(err);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+}
+
+const signup =  async(req, res) => {
+    await sequelize.sync();
+
+    const body = req.body;
+    const rawPassword = body.password;
+    const hash = await bcrypt.hash(rawPassword, saltRounds);
+        
+    const user = User.build({
+        id: uuidv4(),
+        firstName: body.firstName,
+        lastName: body.lastName,
+        email: body.email,
+        password: hash,
+    });
+          
+    await user.save()
+
+    login(req, res);
+}
 
 module.exports = {    
     create: async (req, res) => {
@@ -65,5 +111,7 @@ module.exports = {
         res.writeHead(200, {"Content-Type": "plain/text"});
         res.write("You deleted user with id: " + id);
         res.end();
-    },
+    }, 
+    signup, 
+    login,
 }
